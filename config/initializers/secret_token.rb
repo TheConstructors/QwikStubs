9da7ef93cbaa671f186e15e002ca895fd2b79b88
Jared Roesch <roeschinc@gmail.com>
2013-01-25 12:21:25 -0800

    Initial Commit
    Generated a project and pushed the initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QwikStubs::Application.config.secret_token = 'b01609d2c465d32f4d71e962f4dc6d4c443730818cc9d978ff4b4597f003ed88a9e1f4adaed237b92435e8913f9d2b02fab8fd99aa49fe5860cc19b52b71dbdd'
