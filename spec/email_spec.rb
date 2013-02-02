fe1e561acab6ec4d2600aa272870720a3f18cb57
CommanderHamster <alexhamstra@gmail.com>
2013-02-01 23:23:56 -0800

    added some tests

---
+++ spec/email_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe Email do
+  it "should save email" do
+    @email = Email.new(email:"constructors@qwikstubs.owns", password:"thuglyfe",
+                      oath_key:"1234tqgfqreb",salt:"shaker")
+    @email.email == "constructors@qwikstubs.owns"
+    end
+end
