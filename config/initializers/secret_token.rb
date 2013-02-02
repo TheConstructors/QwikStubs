83fc69cf34ce16642d5d33dced538f9d6431f9fe
CommanderHamster <alexhamstra@gmail.com>
2013-02-01 22:23:07 -0800

    initial commit with mongo db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qwikstubs::Application.config.secret_token = 'db200575eedfebd4a7b7626eb0020f0f1b6231903b91f05146c319986923aee2c85759135673a636f3173293f1a018ea554e5ea04bf15089ba82b306dfffd2c7'
