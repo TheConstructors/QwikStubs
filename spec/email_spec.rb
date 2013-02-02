077467853835744b4a93873c35a2923f88335bf5
Brittany Berin <brittany.berin@gmail.com>
2013-02-02 01:26:13 -0800

    added promoter and membership models and basic rspec tests

--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -3,6 +3,6 @@ describe Email do
   it "should save email" do
     @email = Email.new(email:"constructors@qwikstubs.owns", password:"thuglyfe",
                       oath_key:"1234tqgfqreb",salt:"shaker")
-    @email.email == "constructors@qwikstubs.owns"
+    @email.email.should == "constructors@qwikstubs.owns"
     end
 end
