09637f14b8e3ea27e270980ef9f0d79d36464e04
Jared Roesch <roeschinc@gmail.com>
2013-02-06 15:50:34 -0800

    Added tests for User#password=.
    Wrote some tests to ensure that password= is behaving in the way we would expect. Checks to make sure both hashed_pw and salt are set, and that they are unique after each invocation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
+
 describe User do
-  it "should have first and last name" do
-    @user = User.new(first:"Alex",last:"Hamstra")
+  it "should have first, and last names given full_name" do
+    @user = User.new(full_name: "Alex Hamstra")
     @user.first.should == "Alex"
     @user.last.should == "Hamstra"
+  end
+  
+  it "should have both hashed_pw, and salt after setting password" do
+    @user = User.new()
+    @user.password = "a_new_pass"
+    @user.hashed_pw.should != nil
+    @user.salt.should != nil
+  end
+  
+  it "should change both :hashed_pw, and :salt after setting password" do
+    @user = User.new()
+    @user.password = "a_new_pass"
+    first_hash = @user.hashed_pw 
+    first_salt = @user.salt
+    @user.password = "a_another_pass"
+    second_hash = @user.hashed_pw
+    second_salt = @user.salt
+    first_hash.should != second_hash
+    first_salt.should != first_hash
   end
 end
