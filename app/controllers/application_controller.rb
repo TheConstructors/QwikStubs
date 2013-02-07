7ac9297e3c5af7d4ca7993559a3aa5005a71fe04
Jared Roesch <roeschinc@gmail.com>
2013-02-06 18:02:25 -0800

    Messed up the before_filter.
    Now correctly does the redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_filter :authorized?, :only => [:homepage]
+  before_filter :signed_in?, :only => [:homepage]
   protect_from_forgery
 
   def signed_in?
     session[:user_id].present?
-  end
-
-  def authorized?
-    redirect_to new_session_path unless signed_in?
   end
 
   def homepage
