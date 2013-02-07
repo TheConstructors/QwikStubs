bba4bb26a723f69de2c58992134bf6652c0abcf5
Jared Roesch <roeschinc@gmail.com>
2013-02-06 18:35:32 -0800

    added route aliases for sessions, and users.

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,15 @@ Qwikstubs::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   root :to => 'application#homepage'
+
   resources :sessions
+
   resources :users
+
+  # Helpful aliases for routes
+  get '/login' => 'sessions#new'
+
+  get '/register' => 'users#new'
 
   # Sample resource route with options:
   #   resources :products do
