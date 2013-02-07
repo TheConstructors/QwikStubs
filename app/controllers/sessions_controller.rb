bb72d075ead990e1b57f42d900f9987c2d88fe23
Jared Roesch <roeschinc@gmail.com>
2013-02-06 18:53:59 -0800

    fixed the indentation on sessions_controller
    I also finished up more of the login logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,26 @@
 class SessionsController < ApplicationController
-	# Login View
-	def new
-		#if signed_in? redirect_to home_page
-	end
-	
-	# Login HTTP POST logic 
-	def create
-		email = params[:email]
-		password = params[:password]
-		@user = User.find_by_active_email(email)
-		if @user.is_auth? password 
-			session[:user_id] = @user.id
-		else
-			#redirect_to somewhere
-		end
-	end
-	
-	# Logout 
-	def destroy
-		session[:user_id] = nil
-	end
+  # Login View
+  def new
+    #if signed_in? redirect_to home_page
+  end
+  
+  # Login HTTP POST logic 
+  def create
+    email = params[:email]
+    password = params[:password]
+    @user = User.find_by_active_email(email)
+    if @user.nil?
+      flash[:invalid_creds] = true
+      redirect_to new_session_path
+    else 
+      if @user.is_auth?(password)
+        redirect_to sessions_path
+      end
+    end
+  end
+  
+  # Logout 
+  def destroy
+    session[:user_id] = nil
+  end
 end
