eca641ecbf173898b66e765f826d7180515e7b4a
Ben McCurdy <bpmccurdy@gmail.com>
2013-02-08 05:07:28 -0800

    Added Venue View and Controller

--- app/controllers/venue_controller.rb
+++
@@ -1,7 +0,0 @@
-class VenueController < ApplicationController
-
-
-
-
-
-end
---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,41 @@
+class VenuesController < ApplicationController
+
+  def new
+    @venue = Venue.new
+  end 
+
+  def create
+    @venue = Venue.new(params[:venue])
+
+    if @venue.save
+      redirect_to root_path # change to better location later
+    else
+      flash.now[:error] = "Something went wrong - please check your fiels and try again!"
+      render :new
+    end
+  end 
+
+  def list
+    @venues = Venue.all
+  end 
+
+  # Show specific User
+  def show
+    @venue = Venue.find_by_name(params[:name])
+  end
+
+  # Edit a single User (UI)
+  def edit
+  end
+
+  # Edit Logic
+  def update
+  end
+
+  # Delete a User (probably not gonna be used)
+  def destroy
+  end
+
+
+
+end
