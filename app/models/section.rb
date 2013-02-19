1811730187187c4cdc92f2c89bbe67fc20b056bd
Ben McCurdy <bpmccurdy@gmail.com>
2013-02-18 22:46:03 -0800

    added seed_manual and stubs for seating view

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,9 @@
 class Section
   include MongoMapper::Document
   key :name, String
+  key :xpos, Integer
+  key :ypos, Integer
+
   
   #Validations
   validate :name, :presence => true, :uniqueness => true
@@ -9,6 +12,7 @@ class Section
   ensure_index [[:name, 1]], :unique => true
 
   #Relationships
+  has_many :seat
   belongs_to :venue
 
 end
