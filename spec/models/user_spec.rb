d311c7fc16d0738b4475264e6eefc973fa2ac0df
Jared Roesch <roeschinc@gmail.com>
2013-02-19 19:23:20 -0800

    Updated User Spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,26 +8,25 @@ describe User do
     @user.last_name.should  == "Hamstra"
   end
 
-  it "should have both hashed_pw, and salt after setting password" do
+  it "should have both password_digest, and salt after setting password" do
     @user = User.new()
     @user.password = "a_new_pass"
-    @user.hashed_pw.should_not be_blank
+    @user.password_digest.should_not be_blank
     @user.salt.should_not be_blank
   end
 
-  it "should have active_email after creating" do
-    @user = User.new(full_name: "John Smith")
-    @user.email    = "john@gmail.com"
-    @user.active_email.should == "john@gmail.com"
+  it "should have the email in emails" do
+    @user = User.new full_name: "John Smith", email: "john@gmail.com", :password => "dummy_pw"
+    @user.emails.index("john@gmail.com").should_not be_nil
   end
 
-  it "should change both :hashed_pw, and :salt after setting password" do
+  it "should have both different :password_digest, and :salt after setting password" do
     @user = User.new()
     @user.password = "a_new_pass"
-    first_hash = @user.hashed_pw
+    first_hash = @user.password_digest
     first_salt = @user.salt
     @user.password = "a_another_pass"
-    second_hash = @user.hashed_pw
+    second_hash = @user.password_digest
     second_salt = @user.salt
     first_hash.should_not == second_hash
     first_salt.should_not == first_hash
