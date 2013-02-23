9527965d338438452b0d2e617854688a7aeab7ec
Ben McCurdy <bpmccurdy@gmail.com>
2013-02-22 17:25:52 -0800

    renamed homepage to index and updated for Backbone Integration

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Qwikstubs::Application.routes.draw do
   # Helpful aliases for routes
   match 'login'  => 'sessions#new',     as: 'login'
   match 'logout' => 'sessions#destroy', as: 'logout'
-#  match 'create_venue' => 'venues#new', as: 'create_venue'
+  #match 'create_venue' => 'venues#new', as: 'create_venue'
 
   get '/register' => 'users#new'
   get '/venue/create' => 'venues#new'
   get '/venue/show/:name' => 'venues#show'
   get '/venue' => 'venues#list'
 
-  root :to => 'application#homepage'
+  root :to => 'application#index'
 
 end
