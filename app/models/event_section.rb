2fb80934a311375ee8a16a83b7f9670b56b89a6f
Andrew Berls <andrew.berls@gmail.com>
2013-03-04 16:42:23 -0800

    Clarify both_unique with verbose name and .blank?

--- app/models/event_section.rb
+++ app/models/event_section.rb
@@ -2,18 +2,18 @@ class EventSection
   include MongoMapper::Document
 
   key :price, Float
-  
+
   # validations
   validate :price, :presence => true
-  
-  before_save :both_unique
+
+  before_save :validate_event_and_section_unique
 
   #relationships
   belongs_to :event
   belongs_to :section
   many :event_seat
- 
-  def both_unique
-    nil == EventSection.find(event_id: event.id, section_id: section.id)
+
+  def validate_event_and_section_unique
+    EventSection.find(event_id: event.id, section_id: section.id).blank?
   end
 end
