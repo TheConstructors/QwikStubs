0a813591bf1789e44448421509af75c5eb78a0fe
Jared Roesch <roeschinc@gmail.com>
2013-04-07 23:37:46 -0700

    requires RemoteEvents to be loaded before all else

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require twitter/bootstrap
 //= require underscore
 //= require backbone
+//= require RemoteEvents
 //= require qwikstubs
 //= require_tree ../templates
 //= require_tree ./models
