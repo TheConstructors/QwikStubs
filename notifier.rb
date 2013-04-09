4e6ca139e8d4899a04626e0508213b650a6787ea
Jared Roesch <roeschinc@gmail.com>
2013-04-09 03:03:09 -0700

    hardcodes some events for demo purposes

--- notifier.rb
+++ notifier.rb
@@ -1,14 +1,29 @@
 require 'eventmachine'
 require 'debugger'
 require 'em-websocket'
+require 'json'
+
+def log(msg)
+  puts msg
+end
+
+count = 0
 
 EventMachine::WebSocket.start host: "localhost", port: 8080 do |ws|
-  ws.onopen { ws.send "Hello Client!" }
+  #ws.onopen { ws.send "Hello Client!" }
 
   ws.onmessage do |msg|
-    #debugger
-    puts msg
-    ws.send "Dummy" 
+    log msg
+    if msg == 'count:get'
+      response = { event: 'count:set', data: count }.to_json
+      ws.send response
+    elsif msg == 'count:inc'
+      count += 1
+      response = { event: 'count:set', data: count }.to_json
+      ws.send response
+    else 
+      ws.send msg 
+    end
   end
 
   ws.onerror { |e| puts e }
