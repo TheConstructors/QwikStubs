d421a0d5808534f42f071f3c65312be01d7cfc39
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-04-09 23:47:12 -0700

    cleans up some of the tests

--- spec/models/promoter_membership_spec.rb
+++ spec/models/promoter_membership_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe PromoterMembership do
 
   before(:each) do
-        @p_m = FactoryGirl.create(:promoter_membership)
-      end
+    @p_m = FactoryGirl.create(:promoter_membership)
+  end
       
-      it "should have a role" do
-        @p_m.role.should == "admin"
-      end
-      
-      it "should be associated with one user" do
-        @p_m.user.should_not be_nil
-      end
-      
-      it "should be associated with one promoter" do
-        @p_m.promoter.should_not be_nil
-      end
+  it "should have a role" do
+    @p_m.role.should_not be_empty
+  end
+  
+  it "should be associated with one user" do
+    @p_m.user.should_not be_nil
+  end
+  
+  it "should be associated with one promoter" do
+    @p_m.promoter.should_not be_nil
+  end
       
 end
