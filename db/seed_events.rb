5890faf40848c71903efd34a0e3a05d8a651972f
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-04-19 16:13:33 -0700

    adds more sample events to seed data for when @jroesch and I work on pagination

--- db/seed_events.rb
+++ db/seed_events.rb
@@ -7,4 +7,21 @@ Event.create!({:name=>"Alex Against the Machine", :month=>"May", :day=>"28", :ye
 Event.create!({:name=>"Ben and the Boo Boo Bears", :month=>"Jan", :day=>"14", :year=>"2014", :time=>"1:00pm", :venue=>@venue,
               :description=>"In this not creepy at all kid's show, Ben McCurdy wins the hearts of kids and parents nationwide."})
 Event.create!({:name=>"The Kyle Project", :month=>"Jul", :day=>"12", :year=>"2014", :time=>"1:00pm", :venue=>@venue,
-              :description=>"Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks.Kyle sings so quietly so make sure to bring your hearing aids folks."})
+              :description=>"Kyle sings so quietly so make sure to bring your hearing aids folks. Kyle sings so quietly so make sure to bring your hearing aids folks."})
+
+@venue = Venue.find_by_name("The Constructors Arena")
+
+samples = 1..30
+
+samples.each do |n|
+  Event.create!({:name=>"The Pagina Monologues #{n}", 
+                :month=>"April", :day=>"#{n}", :year=>"2013", :time=>"1:00pm", 
+                :venue=>@venue,
+                :description=>"Fake Event #{n} for testing pagination."})
+end 
+samples.each do |n|
+  Event.create!({:name=>"The Pagina Monologues #{n+30}", 
+                :month=>"May", :day=>"#{n}", :year=>"2013", :time=>"1:00pm", 
+                :venue=>@venue,
+                :description=>"Fake Event #{n+30} for testing pagination."})
+end 
