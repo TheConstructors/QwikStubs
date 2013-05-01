ce2848c95323a5889f6f7c8cf898b85abab17948
Jared Roesch <roeschinc@gmail.com>
2013-04-30 17:57:37 -0700

    rename Stat to Status
    because f*** abbreviations

--- app/models/event_seat.rb
+++ app/models/event_seat.rb
@@ -1,13 +1,13 @@
 class EventSeat
   include MongoMapper::Document
   
-  module Stat
+  module Status
     UNSOLD = 0
     RESERVED = 1
     SOLD = 2
   end
 
-  key :status, Integer
+  key :status, Integer, :default => Status::UNSOLD
   
   validates_numericality_of :status, :greater_than => -1
   validates_numericality_of :status, :less_than => 3
