f6dac408a9791ee2519b433c1d2f8440439f5ec8
Brittany Berin <brittany.berin@gmail.com>
2013-05-04 17:31:14 -0700

    fixed event model merge conflict

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,9 @@ class Event
   
   key :name, String
   key :description, String
-  key :day, String
-  key :year, String
-  key :month, String
+  #key :day, String
+  #key :year, String
+  #key :month, String
   key :time, String
   key :date, Date
   validates_presence_of :name
@@ -16,7 +16,7 @@ class Event
   validates_presence_of :date
   #Name and date need to be unique
   validates_uniqueness_of :name, :scope => [:month, :day, :year]
-    
+  
   #Relationships
   belongs_to :venue
   belongs_to :promoter
