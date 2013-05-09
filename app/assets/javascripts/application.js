44a4af83d264aa057a7a93b84abc946a62357a66
Jared Roesch <roeschinc@gmail.com>
2013-05-08 17:01:28 -0700

    fixes pusher require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require twitter/bootstrap
 //= require underscore
 //= require backbone
-//= require pusher
+//= require pusher.min
 //= require qwikstubs
 //= require_tree ../templates
 //= require_tree ./models
