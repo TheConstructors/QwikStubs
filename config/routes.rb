70d17a538e1ca9bc707f6efa4159d1c93035de99
Brittany Berin <brittany.berin@gmail.com>
2013-05-27 18:14:03 -0700

    routing for new charges controller is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Qwikstubs::Application.routes.draw do
 
   resources :users
+  resources :charges
   scope "api" do
     resources :venues
     resources :events
     resources :orders
-    resources :charges
     get '/events/seats/:id' => 'events#seats'
     get '/events/sections/:id' => 'events#sections'
     get '/events/order/reserve/:id/:total' => 'events#order_reserve_mock'
