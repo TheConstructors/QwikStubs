33d768d214e0b595768aef937f590b954acbc9c8
Jared Roesch <roeschinc@gmail.com>
2013-05-31 22:18:31 -0700

    updates to simulator

--- simulator.rb
+++ simulator.rb
@@ -2,15 +2,17 @@ require 'faraday'
 require 'json'
 require 'debugger'
 
-def get_some_seats
-  result = Faraday.post 'http://localhost:3000/api/orders?event_id=51a926fedaba7819880005a1'
+def build_request(event_id)
+  result = Faraday.post "http://localhost:3000/api/orders?event_id=#{event_id}"
   order = JSON.parse(result.body)
   id = order["id"]
-  debugger
-  ticket_request = "http://localhost:3000/api/orders/#{id}.json?type=best&num=1"
-  Faraday.put ticket_request
+  ticket_request = "http://localhost:3000/api/orders/#{id}.json?type=best&num="
+  Proc.new { |i| Faraday.put (ticket_request + i.to_s) }
 end
 
+request = build_request('51a956b3c4cf0040bf0005a1')
+
 (0..100).each do |i|
-  get_some_seats
+  request.call(8)
+  sleep(1)
 end
