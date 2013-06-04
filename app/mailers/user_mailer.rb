69cdb4e1a0f99e6af9be3c7d1ed9283d2709cdc7
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-06-04 04:08:34 -0700

    set up basic confirmation emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,16 +7,21 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url  = "www.qwikstubs.com/login"
 
-    # QR encoding
-    @qr_code  = "http://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=#{@url}&choe=UTF-8"
-    @image = open(@qr_code).read
-    attachments.inline['qr_code.png'] = @image
-
     attachments.inline['logo-tiny.png'] = File.read('app/assets/images/logo-tiny.png')
     mail(:to => @user.get_email, :subject => "Welcome to Qwikstubs!")
   end
 
-  def confirmation_email(email)
+  def confirmation_email(order, email)
+    @order = order
+    @email = email
+    @url   = "www.qwikstubs.com/orders/#{order.id.to_s}/checkin"
 
+    # QR encoding
+    @qr_code  = "http://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=#{@url}&choe=UTF-8"
+    @image = open(@qr_code).read
+    attachments.inline['qr_code.png'] = @image
+
+    attachments.inline['logo-tiny.png'] = File.read('app/assets/images/logo-tiny.png')
+    mail(:to => @email, :subject => "Qwikstubs Confirmation for Order #{@order.order_number}")
   end
 end
